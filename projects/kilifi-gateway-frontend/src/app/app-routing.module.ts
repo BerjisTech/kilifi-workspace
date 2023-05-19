2a082dc1fbc600d65c9af71e982d71eef2c2a9ec
benedict <berjistechnologies@gmail.com>
2023-05-19 18:28:34 +0300

    bootstrap and tailwind setup

--- projects/kilifi-gateway-frontend/src/app/app-routing.module.ts
+++ projects/kilifi-gateway-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'login', component: AuthComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/kilifi-gateway-frontend/src/app/app.module.ts
+++ projects/kilifi-gateway-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AuthComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
